33f36c8153bb1a0f8d656df8c93094981ec9dee2
dthomas23 <87206951+dthomas23@users.noreply.github.com>
2026-07-27 16:40:22 -0500

    Update next.config.mjs for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const repositoryName = "elite-solutions-group";
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  // Generate static HTML, CSS, and JavaScript files
+  output: "export",
+
+  // Helps GitHub Pages serve folder-based routes
+  trailingSlash: true,
+
+  // GitHub Pages cannot run the Next.js image server
+  images: {
+    unoptimized: true,
+  },
+
+  // Your website will initially be hosted under:
+  // dthomas23.github.io/elite-solutions-group
+  basePath: isProduction ? `/${repositoryName}` : "",
+  assetPrefix: isProduction ? `/${repositoryName}` : "",
 };
 
 export default nextConfig;
